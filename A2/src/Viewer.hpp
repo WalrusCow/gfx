66aab2eb5d4384fd2fac66b520811a243d9f2ec6
WalrusCow <willgmcd@gmail.com>
2015-05-28 17:26:51 -0400

    Cleanup

--- A2/src/Viewer.hpp
+++ A2/src/Viewer.hpp
@@ -10,76 +10,75 @@
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 1, 0))
 #include <QOpenGLBuffer>
 #include <QOpenGLVertexArrayObject>
-#else 
+#else
 #include <QGLBuffer>
 #endif
 
 class Viewer : public QGLWidget {
-    
-    Q_OBJECT
 
-public:
-    Viewer(const QGLFormat& format, QWidget *parent = 0);
-    virtual ~Viewer();
-    
-    QSize minimumSizeHint() const;
-    QSize sizeHint() const;
+  Q_OBJECT
 
-    // If you want to render a new frame, call do not call paintGL(),
-    // instead, call update() to ensure that the view gets a paint 
-    // event.
+ public:
+  Viewer(const QGLFormat& format, QWidget *parent = 0);
+  virtual ~Viewer();
 
-    // *** Fill in these functions (in viewer.cpp) ***
+  QSize minimumSizeHint() const;
+  QSize sizeHint() const;
 
-    // Set the parameters of the current perspective projection using
-    // the semantics of gluPerspective().
-    void set_perspective(double fov, double aspect,
-                         double near, double far);
+  // If you want to render a new frame, call do not call paintGL(),
+  // instead, call update() to ensure that the view gets a paint
+  // event.
 
-    // Restore all the transforms and perspective parameters to their
-    // original state. Set the viewport to its initial size.
-    void reset_view();
+  // *** Fill in these functions (in viewer.cpp) ***
 
-protected:
+  // Set the parameters of the current perspective projection using
+  // the semantics of gluPerspective().
+  void set_perspective(double fov, double aspect, double near, double far);
 
-    // Events we implement
+  // Restore all the transforms and perspective parameters to their
+  // original state. Set the viewport to its initial size.
+  void reset_view();
 
-    // Called when GL is first initialized
-    virtual void initializeGL();
-    // Called when our window needs to be redrawn
-    virtual void paintGL();
-    // Called when a mouse button is pressed
-    virtual void mousePressEvent ( QMouseEvent * event );
-    // Called when a mouse button is released
-    virtual void mouseReleaseEvent ( QMouseEvent * event );
-    // Called when the mouse moves
-    virtual void mouseMoveEvent ( QMouseEvent * event );
- 
-    // Draw a line -- call draw_init first!
-    void draw_line(const QVector2D& p1, const QVector2D& p2) ;
+ protected:
 
-    // Set the current colour
-    void set_colour(const QColor& col);
+  // Events we implement
 
-    // Call this before you begin drawing. Width and height are the width
-    // and height of the GL window.
-    void draw_init();
-private:
+  // Called when GL is first initialized
+  virtual void initializeGL();
+  // Called when our window needs to be redrawn
+  virtual void paintGL();
+  // Called when a mouse button is pressed
+  virtual void mousePressEvent(QMouseEvent* event);
+  // Called when a mouse button is released
+  virtual void mouseReleaseEvent(QMouseEvent* event);
+  // Called when the mouse moves
+  virtual void mouseMoveEvent(QMouseEvent* event);
+
+  // Draw a line -- call draw_init first!
+  void draw_line(const QVector2D& p1, const QVector2D& p2) ;
+
+  // Set the current colour
+  void set_colour(const QColor& col);
+
+  // Call this before you begin drawing. Width and height are the width
+  // and height of the GL window.
+  void draw_init();
+ private:
 
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 1, 0))
-    QOpenGLBuffer mVertexBufferObject;
-    QOpenGLVertexArrayObject mVertexArrayObject;
-#else 
-    QGLBuffer mVertexBufferObject;
+  QOpenGLBuffer mVertexBufferObject;
+  QOpenGLVertexArrayObject mVertexArrayObject;
+#else
+  QGLBuffer mVertexBufferObject;
 #endif
 
-    QGLShaderProgram mProgram;
+  QGLShaderProgram mProgram;
 
-    int mColorLocation;
-    
-    // *** Fill me in ***
-    // You will want to declare some more matrices here
-    QMatrix4x4 m_projection;    
+  int mColorLocation;
+
+  // *** Fill me in ***
+  // You will want to declare some more matrices here
+  QMatrix4x4 m_projection;
 };
 
 #endif
