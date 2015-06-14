d0e9bc5c6fe7553fa315e21e503c4c29753c369e
WalrusCow <willgmcd@gmail.com>
2015-06-14 17:09:52 -0400

    Remove dead code

--- A2/src/Movable.cpp
+++ A2/src/Movable.cpp
@@ -5,33 +5,17 @@
 
 #include "xform.hpp"
 
-namespace {
-
-bool isZero(double a) {
-  const double epsilon = 1e-5;
-  return std::abs(a) < epsilon;
-}
-
-constexpr double hypot(double a, double b, double c) {
-  return std::sqrt(a*a + b*b + c*c);
-}
-
-}
-
 void Movable::rotateX(double rad) {
-  // Maintain axes
   auto mat = xRotationMatrix(rad);
   modelMatrix *= mat;
 }
 
 void Movable::rotateY(double rad) {
-  // Maintain axes
   auto mat = yRotationMatrix(rad);
   modelMatrix *= mat;
 }
 
 void Movable::rotateZ(double rad) {
-  // Maintain axes
   auto mat = zRotationMatrix(rad);
   modelMatrix *= mat;
 }
