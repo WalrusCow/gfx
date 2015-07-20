15ef7213a71b1784bdfc2779e2913a9640d00265
WalrusCow <willgmcd@gmail.com>
2015-07-20 12:13:31 -0400

    Randomize in the real domain of the angles

--- proj/src/lights/AreaLight.cpp
+++ proj/src/lights/AreaLight.cpp
@@ -1,5 +1,7 @@
 #include "AreaLight.hpp"
 
+#include <cmath>
+
 // Random generator and distribution
 thread_local std::default_random_engine AreaLight::generator;
 thread_local std::uniform_real_distribution<double> AreaLight::distribution(0, 1);
@@ -12,8 +14,8 @@ AreaLight::AreaLight(
 std::vector<Point3D> AreaLight::getPoints(size_t n) const {
   std::vector<Point3D> points;
   for (; n > 0; n--) {
-    const double theta = getRandom();
-    const double phi = getRandom();
+    const double theta = getRandom() * 2 * M_PI;
+    const double phi = getRandom() * M_PI;
     const double x = position[0] + radius * std::cos(theta) * std::sin(phi);
     const double y = position[1] + radius * std::sin(theta) * std::sin(phi);
     const double z = position[2] + radius * std::cos(phi);
