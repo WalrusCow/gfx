b0911103753b1c32989e047a870de145a83c31c4
WalrusCow <willgmcd@gmail.com>
2015-07-21 16:47:38 -0400

    Glossy floor instead

--- proj/data/glossy.lua
+++ proj/data/glossy.lua
@@ -2,44 +2,48 @@ require('readobj')
 
 scene = gr.node('scene')
 
+grey = gr.material({0.2, 0.2, 0.2}, {0, 0, 0}, 0, 1)
 bw = gr.function_material("bw_squares", {0, 0, 0}, 25, 1)
 bluegreen = gr.function_material("blue_green_squares", {0, 0, 0}, 25, 1)
-mirror = gr.material({.1, .1, .1}, {0.6, 0.6, 0.6}, 15, 1)
+mirror = gr.material({.1, .1, .1}, {0.6, 0.6, 0.6}, 15, 2)
 
 back = gr.mesh('back', readobj('mymesh.obj'))
 back:translate(-10, -10, -20)
 back:scale(20, 20, 1)
-back:set_material(bluegreen)
+back:set_material(grey)
 scene:add_child(back)
 
 floor = gr.mesh('floor', readobj('mymesh.obj'))
 floor:translate(-10, 0, 0)
 floor:rotate('X', -90)
 floor:scale(20, 20, 1)
-floor:set_material(bw)
+floor:set_material(mirror)
 scene:add_child(floor)
 
 s = gr.sphere('sphere')
-s:set_material(mirror)
-s:translate(-1.6, 1, -15)
+s:set_material(bw)
+s:translate(-1.6, 0.75, -15)
 s:scale(0.75, 0.75, 0.75)
+s:rotate('X', 90)
 scene:add_child(s)
 
 s2 = gr.sphere('sphere')
-s2:set_material(mirror)
-s2:translate(1.6, 1, -15)
+s2:set_material(bw)
+s2:translate(1.6, 0.75, -15)
 s2:scale(0.75, 0.75, 0.75)
+s2:rotate('X', 90)
 scene:add_child(s2)
 
 s3 = gr.sphere('sphere')
-s3:set_material(mirror)
-s3:translate(0, 1, -15)
+s3:set_material(bw)
+s3:translate(0, 0.75, -15)
 s3:scale(0.75, 0.75, 0.75)
+s3:rotate('X', 90)
 scene:add_child(s3)
 
 
 SIZE = 128
-camera = {0, 2, -10}
+camera = {0, 1, -10}
 cameraDirection = {0, 0, -1}
 
 cameraLight = gr.light({0, 5, -15}, {0.7, 0.7, 0.7}, {1, 0, 0})
