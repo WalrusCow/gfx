94ff6f1b2ad335fb57510c83da455210a6c41402
WalrusCow <willgmcd@gmail.com>
2015-07-22 04:41:00 -0400

    Trans scenes

--- proj/data/trans.lua
+++ proj/data/trans.lua
@@ -4,16 +4,29 @@ grey = gr.material({0.2, 0.2, 0.2}, {0, 0, 0}, 0, 1)
 yellow = gr.material({0.75, 0.75, 0}, {0, 0, 0}, 15, 1)
 mirror = gr.material({.1, .1, .1}, {0.6, 0.6, 0.6}, 15, 1)
 bw = gr.function_material("bw_squares", {0, 0, 0}, 25, 1)
-clear = gr.material({1, 1, 1}, {0, 0, 0}, 15, 0, 1.55)
-sphere = gr.sphere('sphere')
-sphere:set_material(clear)
-sphere:translate(0, -roomSize / 2 + 2, -10)
-scene:add_child(sphere)
 
+clear = gr.material({1, 1, 1}, {0, 0, 0}, 15, 0.05, 1.55)
+notasclear = gr.material({1, 1, 1}, {0, 0, 0}, 15, 0.333, 1.55)
+evenlessclear = gr.material({1, 1, 1}, {0, 0, 0}, 15, 0.666, 1.55)
+
+s = gr.sphere('sphere')
+s:set_material(clear)
+s:translate(-3, -roomSize / 2 + 2, -10)
+scene:add_child(s)
+
+s2= gr.sphere('s2')
+s2:set_material(notasclear)
+s2:translate(0, -roomSize / 2 + 2, -10)
+scene:add_child(s2)
+
+s3 =gr.sphere('s2')
+s3:set_material(evenlessclear)
+s3:translate(3, -roomSize / 2 + 2, -10)
+scene:add_child(s3)
 --floor:set_material(mirror)
 roof:set_material(grey)
 
-SIZE = 128
+SIZE = 512
 gr.render(scene, 'trans.png', SIZE, SIZE,
           camera, cameraDirection, {0, 1, 0}, 50,
           {0.4, 0.4, 0.4}, {frontTopLeft, backTopRight})
--- proj/data/trans2.lua
+++ proj/data/trans2.lua
@@ -5,20 +5,41 @@ yellow = gr.material({0.75, 0.75, 0}, {0, 0, 0}, 15, 1)
 mirror = gr.material({.1, .1, .1}, {0.6, 0.6, 0.6}, 15, 1)
 bw = gr.function_material("bw_squares", {0, 0, 0}, 25, 1)
 bluegreen = gr.function_material("blue_green_squares", {0, 0, 0}, 25, 1)
-clear = gr.material({1, 1, 1}, {0, 0, 0}, 15, 0, 1.55)
+clear = gr.material({1, 1, 1}, {0, 0, 0}, 15, 0, 1.09)
+clear2 = gr.material({0.75, 0.00, 0.00}, {0.05, 0.05, 0.05}, 15, 0.33, 1.35)
+clear3 = gr.material({1, 1, 1}, {0.05, 0.05, 0.05}, 15, 0.33, 1.35)
 
 sphere = gr.sphere('sphere')
 sphere:set_material(clear)
 sphere:translate(0, -roomSize / 2 + 2, -roomSize * 4 + 4)
 scene:add_child(sphere)
 
-floor:set_material(yellow)
-back:set_material(bluegreen)
+s1 = gr.cube('s1')
+s1:set_material(clear2)
+s1:translate(1, -roomSize / 2 + 3, -roomSize * 4 + 4)
+s1:rotate('X',33)
+s1:rotate('Y',23)
+scene:add_child(s1)
+
+t = gr.torus('t', 0.3)
+t:set_material(clear3)
+t:translate(-0.5, -roomSize / 2 + 2.6, -roomSize * 4 + 8)
+t:rotate('X',33)
+t:rotate('Y',23)
+t:scale(0.3, 0.3, 0.3)
+scene:add_child(t)
+
+
+floor:set_material(grey)
+back:set_material(bw)
 roof:set_material(grey)
+right:set_material(grey)
+left:set_material(grey)
+front:set_material(grey)
 
 camera = {0, -roomSize / 2 + 2, roomSize * -4 + 10}
 
-SIZE = 128
+SIZE = 512
 gr.render(scene, 'trans2.png', SIZE, SIZE,
           camera, cameraDirection, {0, 1, 0}, 50,
           {0.4, 0.4, 0.4}, {frontTopLeft, backTopRight})
