2abbc62c91226534936b283ce83360d65b3316e0
WalrusCow <willgmcd@gmail.com>
2015-07-22 01:15:09 -0400

    Add a chair...

--- proj/data/final_scene.lua
+++ proj/data/final_scene.lua
@@ -2,11 +2,13 @@ require('final_box')
 
 -- Name, KS, Phong, Alpha, refr
 brown = gr.material({0.094, 0.05, 0.003}, {0.00, 0.00, 0.00}, 1000, 1, 1)
-lensM = gr.material({0.2, 0.2, 0.2}, {0.02, 0.02, 0.02}, 1e10, .11, 1.45)
+lensM = gr.material({0.2, 0.2, 0.2}, {0.02, 0.02, 0.02}, 1e10, .11, 1.85)
 magM = gr.material({0, 0, 1}, {0, 0, 0}, 1, 1)
 --gold = gr.material({0.75164, 0.60648, 0.22648}, {0.628281, 0.555802, 0.366065}, 51.2, 1)
 gold = gr.material({0.75164, 0.60648, 0.22648}, {0,0,0}, 51.2, 1)
 suzyM = gr.material({1, 0, 0}, {0, 0, 0}, 1, 1)
+--chairM = gr.material({0.507, 0.507, 0.507}, {0.508, 0.508, 0.508}, 2, 1)
+chairM = gr.material({0.507, 0.507, 0.507}, {0,0,0}, 2, 1)
 
 antFoodM = gr.material({0, 0.75, 0.75}, {0,0,0},1,1)
 antM = gr.material({0.02, 0.02, 0.02}, {0,0,0},1,1)
@@ -61,9 +63,8 @@ suzy:rotate('X', 35)
 suzy:scale(0.6, 0.6, 0.6)
 
 --
--- Box for ant
+-- Ant scene: Box + "food" + ant + magnifying glass
 --
-
 antScene = gr.node('antScene')
 antScene:translate(-1.3, -roomSize / 2, -3)
 antScene:rotate('Y', 45)
@@ -90,35 +91,97 @@ ant:rotate('Z', 90)
 ant:rotate('Y', -90)
 ant:scale(0.03, 0.03, 0.03)
 
-antScene:add_child(mag)
-scene:add_child(antScene)
+mag:translate(0.62, 0.67, 0.9)
+mag:scale(0.45, 0.45, 0.45)
+--antScene:add_child(mag)
+
+--scene:add_child(antScene)
 
 --
--- Ant
+-- A chair
 --
---ant = gr.mesh('ant', readobj('ant.obj'))
---ant:set_material(suzyM)
---ant:rotate('Z', 180)
---ant:scale(0.06, 0.06, 0.06)
---
---a1 = gr.node('a1')
---a1:add_child(ant)
---a1:translate(LEFT - 0.8, BOTTOM - 1, BACK - 0.8)
---a1:translate(unpack(camera))
---a1:translate(-1.9, -.5, -2.3)
+chairScene = gr.node('chairScene')
+chairScene:translate(-1, -roomSize / 2, -4)
+chairScene:scale(0.5, 0.5, 0.5)
 
---a2 = gr.node('a1')
---a2:add_child(ant)
---a2:translate(unpack(camera))
---a2:translate(-1.9, BOTTOM, -2.3)
+armWidth = 0.3
 
+-- Back pillar
+pillarRad = 0.4
+backPillar = gr.cylinder('backPillar')
+backPillar:set_material(chairM)
+backPillar:translate(0, 2.2, 0)
+backPillar:rotate('X', 90)
+backPillar:scale(pillarRad, pillarRad, 2.2)
 
---scene:add_child(a1)
---scene:add_child(a2)
+-- Front pillar
+frontPillar = gr.cylinder('frontPillar')
+frontPillar:set_material(chairM)
+frontPillar:translate(0, 4/3, 3 - pillarRad)
+frontPillar:rotate('X', 90)
+frontPillar:scale(pillarRad, pillarRad, 4/3)
+
+-- Arm of chair
+armSide = gr.cube('armSide')
+armSide:set_material(chairM)
+armSide:scale(armWidth, 2.2, 3 - pillarRad)
+
+-- A side of the chair
+chairArm = gr.node('chairArm')
+chairArm:add_child(backPillar)
+chairArm:add_child(frontPillar)
+chairArm:add_child(armSide)
+
+leftArmCentre = -1.5 + pillarRad/2
+rightArmCentre = 1.5 - pillarRad/2
+
+-- Left side of chair
+leftChairArm = gr.node('leftChairArm')
+leftChairArm:add_child(chairArm)
+leftChairArm:translate(leftArmCentre, 0, 0)
+chairScene:add_child(leftChairArm)
+
+-- Right side of chair
+rightChairArm = gr.node('rightChairArm')
+rightChairArm:add_child(chairArm)
+rightChairArm:translate(rightArmCentre, 0, 0)
+chairScene:add_child(rightChairArm)
+
+-- Back of chair
+back = gr.cube('chairBack')
+chairScene:add_child(back)
+back:set_material(chairM)
+back:translate(-1.5, 0, 0)
+back:scale(3, 4, armWidth)
+
+-- Base of chair
+base = gr.cube('base')
+chairScene:add_child(base)
+base:translate(leftArmCentre, 0, 0)
+base:set_material(chairM)
+base:scale(3 - pillarRad, 1, 3 - 1.5*pillarRad)
+
+-- Chair step
+step = gr.cube('step')
+chairScene:add_child(step)
+step:translate(leftArmCentre, 0, 3 - pillarRad)
+step:set_material(chairM)
+step:scale(3 - pillarRad, 0.5, pillarRad/2)
+
+-- Chair seat
+seat = gr.cube('seat')
+chairScene:add_child(seat)
+seat:set_material(chairM)
+seat:translate(-0.6, 1, 0)
+seat:scale(1.2, 0.8, 1.8)
+
+chairScene:rotate('Y', 35)
+scene:add_child(chairScene)
+
 
 cameraDirection = {-2, -0.8, -2}
 
-SIZE = 64
+SIZE = 128
 gr.render(scene, 'final_scene.png', SIZE, SIZE,
           camera, cameraDirection, {0, 1, 0}, 50,
           {0.4, 0.4, 0.4}, {frontTopLeft, backTopRight})
