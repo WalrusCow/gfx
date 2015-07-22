a8c1ed51e442bc764eb7dee13b966f8672600158
WalrusCow <willgmcd@gmail.com>
2015-07-22 03:22:50 -0400

    Final cylinder scene

--- proj/data/cyl.lua
+++ proj/data/cyl.lua
@@ -5,6 +5,7 @@ blue = gr.material({0.25, 0.33, 0.88}, {0.5, 0.4, 0.8}, 25, 1)
 cylinder = gr.cylinder('cylinder')
 scene:add_child(cylinder)
 cylinder:set_material(blue)
+cylinder:rotate('Z', 25)
 cylinder:rotate('X', 90)
 cylinder:translate(-2.5, 0, 0)
 --scene:rotate('Y', 45)
@@ -13,6 +14,7 @@ require('cylinder')
 scene:add_child(cyl)
 cyl:set_material(blue)
 cyl:translate(2.5, -2, 0)
+cyl:rotate('Z', 25)
 cyl:scale(1, 4, 1)
 
 l1 = gr.light({0, 0, 10}, {0.8, 0.8, 0.8}, {1, 0, 0})
@@ -23,7 +25,7 @@ for i = 1, 36 do
   --cylinder:rotate('X', 10) -- scale has to be last or else things get fckd
   --cylinder:scale(1, 1, 2)
   scene:rotate('X', 10)
-  gr.render(scene, i..'cyl.png', 256, 256,
+  gr.render(scene, i..'cyl.png', 512, 512,
       {0, 0, 10}, {0, 0, -1}, {0, 1, 0}, 50,
       {0.4, 0.4, 0.4}, {l1})
 end
